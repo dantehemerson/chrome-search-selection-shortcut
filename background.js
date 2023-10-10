566f231ed172f016fb4926ed211837b486d843e1
Dante Calderon <18385321+dantehemerson@users.noreply.github.com>
2023-10-09 22:40:35 -0500

    if it's hacky and you know it clap you hands (clap clap)!

--- background.js
+++ background.js
@@ -14,37 +14,6 @@ async function getCurrentTab() {
   return tabs[0];
 }
 
-// TODO: Se way to get selected text from PDF
-function getPdfSelectedText() {
-  // get PDF selected text in chrome internal plugin
-  // refer to: https://stackoverflow.com/questions/61076303/how-can-i-get-selected-text-in-pdf-in-javascript
-  return new Promise((resolve) => {
-    window.addEventListener("message", function onMessage(e) {
-      if (
-        e.origin === "chrome-extension://mhjfbmdgcfjbbpaeojofohoefgiehjai" &&
-        e.data &&
-        e.data.type === "getSelectedTextReply"
-      ) {
-        window.removeEventListener("message", onMessage);
-        resolve(e.data.selectedText);
-      }
-    });
-    // runs code in page context to access postMessage of the embedded plugin
-    const script = document.createElement("script");
-    if (chrome.runtime.getManifest().manifest_version > 2) {
-      script.src = chrome.runtime.getURL("query-pdf.js");
-    } else {
-      script.textContent = `(${() => {
-        document
-          .querySelector("embed")
-          .postMessage({ type: "getSelectedText" }, "*");
-      }})()`;
-    }
-    document.documentElement.appendChild(script);
-    script.remove();
-  });
-}
-
 function injection() {
   //console.log("injection")
   // get DOM selected text
